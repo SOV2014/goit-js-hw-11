f77bc36f0c56804727108310dd27634d456c4c2d
Oleksandr Savych <159421282+SOV2014@users.noreply.github.com>
2024-07-21 12:38:58 +0200

    hw-11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,14 @@
+const API_KEY = '44780398-b9dbe1b89370a8f5261d05d93';
+
+export function getPicturesByQuery(query) {
+  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(
+    query
+  )}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  return fetch(url).then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  });
+}
